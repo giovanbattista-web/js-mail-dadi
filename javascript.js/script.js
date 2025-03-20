eee8b77965d8543d54d980e2acf093ca48f230d2
giovanbattista-web <giovanbattistadamico1994@gmail.com>
2025-03-20 15:44:13 +0100

    [UPD] CONTROLLO E STAMPA SULL'E-MAIL

--- javascript.js/script.js
+++ javascript.js/script.js
@@ -1,2 +1,14 @@
 const email = ['giovanni@gmail.com', 'lorenzo@outlook.it', 'maurizio@outlook.com', 'nicola@libero.it'];
 let inserimentoUtente = prompt('inserisci la tua e-mail');
+let controllo = false;
+for (let i = 0; controllo === false && i < email.length; i++) {
+    if (email[i] === inserimentoUtente) {
+        controllo = true;
+    }
+}
+if (controllo == true) {
+    console.log('e-mail trovata');
+}
+else {
+    console.log('e-mail non trovata')
+}
